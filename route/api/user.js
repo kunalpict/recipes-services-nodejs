aa61804da91ee63fd47c97363a4f38b82d21f0ec
kunal Chaudhari <kchaudhari@scrippsnetworks.com>
2016-05-05 13:07:01 -0400

    added apis

--- route/api/user.js
+++ route/api/user.js
@@ -12,7 +12,7 @@ router.post('/user/login', function(req, res, next) {
           expiresIn: 30 // expires in 24 hours
         });
         console.log(token);
-        setTimeout(function(){
+        /*setTimeout(function(){
           jwt.verify(token, "thisistokentest", function(err, decoded) {      
             
             if (err) {
@@ -24,9 +24,10 @@ router.post('/user/login', function(req, res, next) {
               
             }
           });
-        },60);
-        //res.cookie('api-token', token);
-        //res.sendStatus(200);
+        },60);*/
+
+        res.cookie('api-token', token);
+        res.sendStatus(200);
       } else {
         res.sendStatus(403);
       }
@@ -41,13 +42,22 @@ router.get('/user/check', function(req, res, next) {
 });
 
 router.post('/user/create', function(req, res, next) {
+
+  console.log(req.body)
+   auth.create({
+          userId : req.body.username,
+          pwd : req.body.password,
+          firstName : req.body.firstname,
+          lastName : req.body.lastname});
+
+   res.sendStatus(200);
   
   /*var pwd = crypto
       .createHash("md5")
       .update(req.body.password)
       .digest('hex');*/
 
-  crypto.randomBytes(128, function (err, salt) {
+/*  crypto.randomBytes(128, function (err, salt) {
     if (err) { throw err; }
     salt = new Buffer(salt).toString('hex');
     crypto.pbkdf2(req.body.password, salt, 1000, 256, 
@@ -62,7 +72,7 @@ router.post('/user/create', function(req, res, next) {
         res.sendStatus(200);
         console.log((new Buffer(hash).toString('hex')));
       });
-  });    
+  });  */  
 
  
 });
--- route/auth/auth.js
+++ route/auth/auth.js
@@ -3,74 +3,74 @@ var mysqlConfig = require('../config/mysqlconfig');
 
 var auth = function() {
 
-	function create(user){
-		var promise = new Promise(function(resolve, reject){
-			var connection = mysqlConfig.create(); 
-    		connection.connect();
-    		connection.query('INSERT INTO `study`.`user` SET ?',
-    			user,
-    			function(err, results) 
-    		{
-    			if (err) reject(err);
-   				resolve(results);      		
-    		});
- 
-    		connection.end();  
-		});
+    function create(user) {
+        var promise = new Promise(function(resolve, reject) {
+            var connection = mysqlConfig.create();
+            connection.connect();
+            connection.query('INSERT INTO `study`.`user` SET ?',
+                user,
+                function(err, results) {
+                    console.log(err);
+                    if (err) reject(err);
+                    resolve(results);
+                });
 
-		return promise;
-	}
+            connection.end();
+        });
 
-	function check(username){
-		var promise = new Promise(function(resolve, reject){
-			var connection = mysqlConfig.create(); 
-    		connection.connect();
-    		connection.query('SELECT * FROM study.user where userId = ?',
-    			[username],
-    			function(err, results) {
-    			console.log(err);
-    			if (err) reject(err);
+        return promise;
+    }
 
-   				resolve(results);      		
-    		});
- 			connection.end();  
-		});
+    function check(username) {
+        var promise = new Promise(function(resolve, reject) {
+            var connection = mysqlConfig.create();
+            connection.connect();
+            connection.query('SELECT * FROM study.user where userId = ?', [username],
+                function(err, results) {
+                    console.log(err);
+                    if (err) reject(err);
 
-		return promise;
-	}
+                    resolve(results);
+                });
+            connection.end();
+        });
 
-	function login(username, password){
-		var promise = new Promise(function(resolve, reject){
-			var connection = mysqlConfig.create(); 
-    		connection.connect();
-    		connection.query('SELECT * FROM study.user where userId = ? and pwd = ?',
-    			[username, password],
-    			function(err, results) 
-    		{
-    			if (err) reject(err);
-   				resolve(results);      		
-    		});
- 
-    		connection.end();  
-		});
+        return promise;
+    }
 
-		return promise;
-	}
+    function login(username, password) {
+        console.log(username+"  "+password)
+        var promise = new Promise(function(resolve, reject) {
+            var connection = mysqlConfig.create();
+            connection.connect();
+            connection.query('SELECT * FROM study.user where userId = ? and pwd = ?', [username, password],
+                function(err, results) {
+                    console.log(results)
+                    if (err) reject(err);
+                    resolve(results);
+                });
 
-	function logout(username){
-		
-	}
-	function deleteUser(username){
-		
-	}
+            connection.end();
+        });
 
-	return {
-		create: create,
-		check: check,
-		login: login,
-		logout: logout,
-		deleteUser: deleteUser
-	};
+        return promise;
+    }
+
+    function logout(username) {
+
+    }
+
+    function deleteUser(username) {
+
+    }
+
+    return {
+        create: create,
+        check: check,
+        login: login,
+        logout: logout,
+        deleteUser: deleteUser
+    };
 };
 
 module.exports = auth();
