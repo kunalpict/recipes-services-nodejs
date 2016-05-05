879cad3777f5832d80adb0895e97d4454da318ba
kunal Chaudhari <kchaudhari@scrippsnetworks.com>
2016-05-05 16:27:06 -0400

    dded logic to call multiselect queries

--- route/api/recipe.js
+++ route/api/recipe.js
@@ -6,84 +6,109 @@ var router = express.Router();
 var mysqlConfig = require('../config/mysqlconfig');
 /* GET home page. */
 router.get('/init', function(req, res, next) {
-  console.log(req.cookies.rememberme);
-  console.log(uuid.v4());
-  console.log(uuid.v1());
+    console.log(req.cookies.rememberme);
+    console.log(uuid.v4());
+    console.log(uuid.v1());
 });
 
 router.post('/recipe/new', function(req, res, next) {
-  var id = uuid.v4();
-  console.log(JSON.stringify(req.body.procedure));
-    var connection = mysqlConfig.create(); 
-      connection.connect();
-      connection.query('INSERT INTO study.recipes (id,title,category,user,cook_time,date,description,image) values (?,?,?,?,?,?,?,?)',
-        [id,req.body.title,req.body.category,req.body.user,req.body.cook_time,
-        req.body.date,req.body.desc,req.body.image],
-        function(err, results) 
-      {
-        if (err) throw err;
-      });
+    var id = uuid.v4();
+    console.log(JSON.stringify(req.body.procedure));
+    var connection = mysqlConfig.create();
+    connection.connect();
+    connection.query('INSERT INTO study.recipes (id,title,category,user,cook_time,date,description,image) values (?,?,?,?,?,?,?,?)', [id, req.body.title, req.body.category, req.body.user, req.body.cook_time,
+            req.body.date, req.body.desc, req.body.image
+        ],
+        function(err, results) {
+            if (err) throw err;
+        });
 
-      // connection.query('INSERT INTO study.ingredients (id, ingredient) values (?,?)',
-      //   [id, JSON.stringify(req.body.ingredient)],
-      //   function(err, results) 
-      // {
-      //   if (err) throw err;
-      // });
+    // connection.query('INSERT INTO study.ingredients (id, ingredient) values (?,?)',
+    //   [id, JSON.stringify(req.body.ingredient)],
+    //   function(err, results) 
+    // {
+    //   if (err) throw err;
+    // });
 
-/*      var sql = connection.query('INSERT INTO study.procedures (id, procedure) values (?,?)',
-        [id, JSON.stringify(req.body.procedure)],
-        function(err, results) 
-      {
-        if (err) throw err;
-      });
+    /*      var sql = connection.query('INSERT INTO study.procedures (id, procedure) values (?,?)',
+            [id, JSON.stringify(req.body.procedure)],
+            function(err, results) 
+          {
+            if (err) throw err;
+          });
 
-      console.log(sql);
-*/      connection.end();  
-    
+          console.log(sql);
+    */
+    connection.end();
 
-     res.sendStatus(200)
+
+    res.sendStatus(200)
 });
 
 router.get('/recipe/:id', function(req, res, next) {
-   /* redisClient.client.getAsync(req.params.id)
-    .then(function(data) {
-      if (data) {
-        console.log(data);
-        res.json(JSON.parse(data));
-      } else 
-      {*/ 
-        var connection = mysqlConfig.create(); 
+    var promise1 = new Promise(function(resolve, reject) {
+        var connection = mysqlConfig.create();
         connection.connect();
-        connection.query('SELECT * FROM study.recipes where id = ?',
-        [req.params.id],  
-        function(err, rows, fields) {
-          
-          if (err) console.log(err);
-         /* rows.forEach( function(element, index) {
-            //element.ingredient = JSON.parse(element.ingredient);
-            //element.procedure = JSON.parse(element.procedure);
-          });*/
-          res.json(rows);
-          //redisClient.client.set(req.params.id, JSON.stringify(rows));
-         // redisClient.client.expire(req.params.id, 30);
-          connection.end();
-        });
-      /*}
-     });*/
+        connection.query('SELECT * FROM study.recipes where id = ?', [req.params.id],
+            function(err, rows, fields) {
+
+                if (err) console.log(err);
+                resolve(rows);
+                connection.end();
+            });
+    });
+
+    var promise2 = new Promise(function(resolve, reject) {
+        var connection = mysqlConfig.create();
+        connection.connect();
+        connection.query('SELECT ingredient, qty FROM study.ingredients where id = ?', [req.params.id],
+            function(err, rows, fields) {
+
+                if (err) console.log(err);
+                resolve(rows);
+                connection.end();
+            });
+    });
+
+
+    var promise3 = new Promise(function(resolve, reject) {
+        var connection = mysqlConfig.create();
+        connection.connect();
+        connection.query('SELECT * FROM study.procedures where id = ? order by step;', [req.params.id],
+            function(err, rows, fields) {
+
+                if (err) console.log(err);
+                resolve(rows);
+                connection.end();
+            });
+    });
+
+
+    Promise.all([promise1, promise2, promise3])
+        .then(function(resolve) {
+            console.log(resolve[0]);
+            
+            var jsonResponse = {
+              info: resolve[0][0],
+              ingredients: resolve[1],
+              directions: resolve[2]
+            }
+
+            res.json(jsonResponse);
+        })
 });
 
 router.get('/recipes', function(req, res, next) {
-    var connection = mysqlConfig.create(); 
+    var connection = mysqlConfig.create();
     connection.connect();
     connection.query('SELECT * FROM study.recipes', function(err, rows, fields) {
-    if (err) console.log(err);
-      res.cookie('name', 'tobi', { domain: 'localhost', path: '/api/init', secure: true });
-      res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
-      res.json(rows);
+        if (err) console.log(err);
+        res.cookie('name', 'tobi', { domain: 'localhost', path: '/api/init', secure: true });
+        res.cookie('rememberme', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
+        res.json(rows);
     });
- 
-    connection.end(); 
+
+    connection.end();
 });
 
 module.exports = router;
